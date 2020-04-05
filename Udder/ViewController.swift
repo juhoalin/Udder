29a5a65a93f6a371f689feb32c3b6e67b932e14a
Juho Alin <juhoalin@icloud.com>
2020-04-05 16:03:53 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Juho Alin on 5.4.2020.
+//  Copyright © 2020 Juho Alin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
